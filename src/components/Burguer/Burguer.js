a1a1dd7868826fcef3d4479b3d800606c3d66233
Servando J Berna Rico <servandojbernarico@Servandos-MacBook-Pro.local>
2018-10-01 16:51:32 -0700

    Burguer rendering dynamically.

--- src/components/Burguer/Burguer.js
+++ src/components/Burguer/Burguer.js
@@ -3,14 +3,26 @@ import classes from "./Burguer.css";
 import Ingredient from "./Ingredient/Ingredient";
 
 const burger = props => {
+  let transfromedIngredients = Object.keys(props.ingredients)
+    .map(igKey => {
+      return [...Array(props.ingredients[igKey])].map((_, i) => {
+        return <Ingredient key={igKey + i} type={igKey} />;
+      });
+    })
+    .reduce((arr, el) => {
+      return arr.concat(el);
+    }, []);
+
+  if (transfromedIngredients.length === 0) {
+    transfromedIngredients = <p>Please start adding ingredients!</p>;
+  }
   return (
     <div className={classes.Burguer}>
       <Ingredient type="bread-top" />
-      <Ingredient type="cheese" />
-      <Ingredient type="meat" />
+      {transfromedIngredients}
       <Ingredient type="bread-bottom" />
     </div>
   );
-}
+};
 
 export default burger;
