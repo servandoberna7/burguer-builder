427fcbce83b8f39f46f8f428f8bb1a553742a53d
Servando J Berna Rico <servandojbernarico@Servandos-MacBook-Pro.local>
2018-10-02 21:06:18 -0700

    Menu and SideDrawer completed.

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,15 +1,31 @@
-import React from "react";
+import React, { Component } from "react";
 import Aux from "../../hoc/Aux";
 import classes from "./Layout.css";
 import Toolbar from "../Navigation/Tooldbar/Toolbar";
 import SideDrawer from "../Navigation/SideDrawer/SideDrawer";
 
-const layout = props => (
-  <Aux>
-    <Toolbar />
-    <SideDrawer />
-    <main className={classes.Content}>{props.children}</main>
-  </Aux>
-);
+class Layout extends Component {
+  state = {
+    showSideDrawer: false
+  };
 
-export default layout;
+  sideDrawerHandler = () => {
+    const reverse = !this.state.showSideDrawer;
+    this.setState({ showSideDrawer: reverse });
+  };
+
+  render() {
+    return (
+      <Aux>
+        <Toolbar click={this.sideDrawerHandler} />
+        <SideDrawer
+          open={this.state.showSideDrawer}
+          closed={this.sideDrawerHandler}
+        />
+        <main className={classes.Content}>{this.props.children}</main>
+      </Aux>
+    );
+  }
+}
+
+export default Layout;
--- src/components/Navigation/Tooldbar/Toolbar.js
+++ src/components/Navigation/Tooldbar/Toolbar.js
@@ -2,14 +2,15 @@ import React from "react";
 import classes from "./Toolbar.css";
 import Logo from "../../Logo/Logo";
 import NavigationItems from "../NavigationItems/NavigationItems";
+import Menu from "../Menu/Menu";
 
 const toolbar = props => (
   <header className={classes.Toolbar}>
-    <div>MENU</div>
+    <Menu click={props.click}/>
     <div className={classes.Logo}>
       <Logo />
     </div>
-    <nav>
+    <nav className={classes.DesktopOnly}>
       <NavigationItems />
     </nav>
   </header>
